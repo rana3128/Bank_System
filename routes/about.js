e0f8bcf951c78825581c408b498cda3d87c0cf67
vijay <vijay.pratap@nomissolutions.com>
2021-01-28 11:43:46 +0530

    added by user-1 in its own branch

--- routes/about.js
+++ routes/about.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const db = require("./config/mongodb");
 const tablename = 'user_account';
 
+
+// this is added by real user 1 in its own branch
+
 router.post('/', function(req, res) {
     //send static data
     res.status(200);
